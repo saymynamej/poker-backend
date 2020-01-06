e5cd7dbdc236e8b8dd04b3e60549184d60e2c285
saymynamej <h2so478521>
2020-01-06 14:57:27 +0200

    extracted method for coping RoundSettingsDTO

--- src/main/java/ru/sm/poker/util/RoundSettingsUtil.java
+++ src/main/java/ru/sm/poker/util/RoundSettingsUtil.java
@@ -2,22 +2,39 @@ package ru.sm.poker.util;
 
 import ru.sm.poker.dto.RoundSettingsDTO;
 import ru.sm.poker.model.Player;
+import ru.sm.poker.model.action.Wait;
 
 import java.util.Collections;
 import java.util.List;
 
 public class RoundSettingsUtil {
 
+
+    public static RoundSettingsDTO copyWithNewPlayersAndReloadAction(RoundSettingsDTO roundSettingsDTO, List<Player> players) {
+        final String gameName = players.get(0)
+                .getGameName();
+
+        players.forEach(player -> player.setAction(new Wait(gameName)));
+
+        return copy(roundSettingsDTO, players);
+    }
+
+
     public static RoundSettingsDTO copyWithSecureCard(RoundSettingsDTO roundSettingsDTO, String filterName) {
 
         final List<Player> playersWithSecureCards = PlayerUtil.copies(roundSettingsDTO.getPlayers());
 
-        playersWithSecureCards.forEach(player ->  {
-            if (!player.getName().equals(filterName)){
+        playersWithSecureCards.forEach(player -> {
+            if (!player.getName().equals(filterName)) {
                 player.addCards(Collections.emptyList());
             }
         });
 
+        return copy(roundSettingsDTO, playersWithSecureCards);
+    }
+
+
+    private static RoundSettingsDTO copy(RoundSettingsDTO roundSettingsDTO, List<Player> players) {
         return RoundSettingsDTO.builder()
                 .gameName(roundSettingsDTO.getGameName())
                 .bank(roundSettingsDTO.getBank())
@@ -26,7 +43,7 @@ public class RoundSettingsUtil {
                 .bigBlind(roundSettingsDTO.getBigBlind())
                 .smallBlind(roundSettingsDTO.getSmallBlind())
                 .button(roundSettingsDTO.getButton())
-                .players(playersWithSecureCards)
+                .players(players)
                 .stageType(roundSettingsDTO.getStageType())
                 .lastBet(roundSettingsDTO.getLastBet())
                 .activePlayer(roundSettingsDTO.getActivePlayer())
@@ -35,5 +52,4 @@ public class RoundSettingsUtil {
                 .tern(roundSettingsDTO.getTern())
                 .build();
     }
-
 }
