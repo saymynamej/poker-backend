bc1579adb3ff072c4d5e3dd12e29429dc0f7ee2f
saymynamej <h2so478521>
2020-01-12 03:04:23 +0300

    refactored

--- src/test/java/ru/sm/poker/action/CallCommonStrategyTest.java
+++ src/test/java/ru/sm/poker/action/CallCommonStrategyTest.java
@@ -12,12 +12,11 @@ import ru.sm.poker.action.holdem.Call;
 import ru.sm.poker.action.strategy.ActionStrategy;
 import ru.sm.poker.action.strategy.call.CallCommonStrategy;
 import ru.sm.poker.dto.RoundSettingsDTO;
-import ru.sm.poker.enums.RoleType;
 import ru.sm.poker.model.Player;
 import ru.sm.poker.service.ActionService;
 
-import java.util.Collections;
-import java.util.Random;
+import static ru.sm.poker.util.DTOUtilTest.getPlayer;
+import static ru.sm.poker.util.DTOUtilTest.getRoundSettingsDTO;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -29,7 +28,7 @@ class CallCommonStrategyTest {
 
     private ActionStrategy actionStrategy = new CallCommonStrategy();
 
-    private String gameName = "test";
+    private final static String GAME_NAME = "test";
 
     private final static long FULL_CHIPS_COUNT = 5000L;
     private final static long BET = 100L;
@@ -37,35 +36,20 @@ class CallCommonStrategyTest {
 
     @Test
     void testSuccessStrategy() {
-        final RoundSettingsDTO roundSettingsDTO = getRoundSettingsDTO();
-        final Player player = getPlayer();
-        actionStrategy.execute(player, actionService, new Call(BET, gameName), roundSettingsDTO);
+        final RoundSettingsDTO roundSettingsDTO = getRoundSettingsDTO(BET);
+        final Player player = getPlayer(GAME_NAME, FULL_CHIPS_COUNT);
+        actionStrategy.execute(player, actionService, new Call(BET, GAME_NAME), roundSettingsDTO);
         Assertions.assertEquals(player.getChipsCount(), EXPECTED_CHIPS_COUNT_AFTER_BET);
         Assertions.assertEquals(roundSettingsDTO.getBank(), BET);
     }
 
     @Test
     void testFailStrategy() {
-        final RoundSettingsDTO roundSettingsDTO = getRoundSettingsDTO();
-        roundSettingsDTO.setLastBet(roundSettingsDTO.getLastBet() + new Random().nextInt(10));
-        final Player player = getPlayer();
-        actionStrategy.execute(player, actionService, new Call(BET, gameName), roundSettingsDTO);
+        final RoundSettingsDTO roundSettingsDTO = getRoundSettingsDTO(BET);
+        roundSettingsDTO.setLastBet(roundSettingsDTO.getLastBet() + 1);
+        final Player player = getPlayer(GAME_NAME, FULL_CHIPS_COUNT);
+        actionStrategy.execute(player, actionService, new Call(BET, GAME_NAME), roundSettingsDTO);
         Mockito.verify(actionService).waitPlayerAction(player, roundSettingsDTO);
     }
 
-    public RoundSettingsDTO getRoundSettingsDTO() {
-        return RoundSettingsDTO.builder()
-                .lastBet(BET)
-                .players(Collections.emptyList())
-                .build();
-    }
-
-
-    public Player getPlayer() {
-        return Player.builder()
-                .gameName(gameName)
-                .chipsCount(FULL_CHIPS_COUNT)
-                .roleType(RoleType.PLAYER)
-                .build();
-    }
 }
