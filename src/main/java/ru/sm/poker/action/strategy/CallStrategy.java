157b0054dab07155327faef8c005bd144e70db44
saymynamej <h2so478521>
2020-01-23 13:50:13 +0300

    repaired logic for call

--- src/main/java/ru/sm/poker/action/strategy/CallStrategy.java
+++ src/main/java/ru/sm/poker/action/strategy/CallStrategy.java
@@ -6,12 +6,18 @@ import ru.sm.poker.model.Player;
 import ru.sm.poker.service.ActionService;
 import ru.sm.poker.service.common.GameService;
 
-import static ru.sm.poker.util.HistoryUtil.*;
+import static ru.sm.poker.util.HistoryUtil.sumAllHistoryBetsWithNewAction;
+import static ru.sm.poker.util.PlayerUtil.checkPlayerHasEnoughChips;
 
 public class CallStrategy implements ActionStrategy {
 
     @Override
     public void execute(Player player, GameService gameService, ActionService actionService, CountAction countAction, RoundSettingsDTO roundSettingsDTO) {
+        if (!checkPlayerHasEnoughChips(player, countAction)) {
+            actionService.waitUntilPlayerWillHasAction(player, roundSettingsDTO);
+            return;
+        }
+
         final long bets = sumAllHistoryBetsWithNewAction(roundSettingsDTO, player, countAction);
 
         if (bets == roundSettingsDTO.getLastBet()) {
