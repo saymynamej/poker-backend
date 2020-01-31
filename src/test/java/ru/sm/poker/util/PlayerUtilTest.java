ff4b9c1d7b0df61a82c20f00957ecde84d47b2a1
saymynamej <h2so478521>
2020-01-31 12:48:11 +0300

    removed imports

--- src/test/java/ru/sm/poker/util/PlayerUtilTest.java
+++ src/test/java/ru/sm/poker/util/PlayerUtilTest.java
@@ -2,10 +2,6 @@ package ru.sm.poker.util;
 
 import org.junit.Before;
 import org.junit.Test;
-import ru.sm.poker.dto.PlayerDTO;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class PlayerUtilTest  {
 
@@ -19,7 +15,6 @@ public class PlayerUtilTest  {
 
     @Test
     public void getPlayersInGame (){
-
     }
 
 }
