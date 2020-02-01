2844e09682b3004de4444b824252cad76909bf0e
saymynamej <h2so478521>
2020-02-01 14:32:29 +0300

    added test for check action

---
+++ src/test/java/ru/sm/poker/action/CheckTest.java
@@ -0,0 +1,76 @@
+package ru.sm.poker.action;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.sm.poker.action.holdem.Check;
+import ru.sm.poker.dto.HoldemRoundSettingsDTO;
+import ru.sm.poker.dto.PlayerDTO;
+import ru.sm.poker.enums.RoleType;
+import ru.sm.poker.enums.StageType;
+import ru.sm.poker.service.ActionService;
+import ru.sm.poker.service.common.GameService;
+import ru.sm.poker.util.DTOUtilTest;
+import ru.sm.poker.util.WaitUtil;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class CheckTest {
+
+
+    @SpyBean
+    private GameService gameService;
+
+    @SpyBean
+    private ActionService actionService;
+
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    @Test
+    public void testSuccessCheckWhenLastBetZero() {
+        final HoldemRoundSettingsDTO roundSettingsDTO = DTOUtilTest.getRoundSettingsDTO(StageType.FLOP);
+        System.out.println(roundSettingsDTO.getStageType());
+        roundSettingsDTO.setLastBet(0L);
+        final PlayerDTO player = DTOUtilTest.getPlayer();
+        new Check().doAction(roundSettingsDTO, player, gameService, actionService);
+    }
+
+    @Test
+    public void testFailCheckWhenLastBetZero() {
+        final HoldemRoundSettingsDTO roundSettingsDTO = DTOUtilTest.getRoundSettingsDTO(StageType.PREFLOP);
+        System.out.println(roundSettingsDTO.getStageType());
+        roundSettingsDTO.setLastBet(0L);
+        final PlayerDTO player = DTOUtilTest.getPlayer();
+        executorService.submit(() -> new Check().doAction(roundSettingsDTO, player, gameService, actionService));
+        WaitUtil.waitOfCondition(() -> player.getAction() != null);
+        Mockito.verify(actionService, Mockito.times(1)).waitUntilPlayerWillHasAction(player, roundSettingsDTO);
+    }
+
+    @Test
+    public void testSuccessCheckWhenPlayerOnBigBlind() {
+        final HoldemRoundSettingsDTO roundSettingsDTO = DTOUtilTest.getRoundSettingsDTO(StageType.PREFLOP);
+        roundSettingsDTO.setLastBet(2L);
+        final PlayerDTO player = DTOUtilTest.getPlayer();
+        player.setRole(RoleType.BIG_BLIND);
+        new Check().doAction(roundSettingsDTO, player, gameService, actionService);
+    }
+
+    @Test
+    public void testFailCheckWhenPlayerOnBigBlind() {
+        final HoldemRoundSettingsDTO roundSettingsDTO = DTOUtilTest.getRoundSettingsDTO(StageType.PREFLOP);
+        roundSettingsDTO.setLastBet(roundSettingsDTO.getBigBlindBet() + roundSettingsDTO.getBigBlindBet());
+        final PlayerDTO player = DTOUtilTest.getPlayer();
+        executorService.submit(() -> new Check().doAction(roundSettingsDTO, player, gameService, actionService));
+        WaitUtil.waitOfCondition(() -> player.getAction() != null);
+        Mockito.verify(actionService, Mockito.times(1)).waitUntilPlayerWillHasAction(player, roundSettingsDTO);
+    }
+}
