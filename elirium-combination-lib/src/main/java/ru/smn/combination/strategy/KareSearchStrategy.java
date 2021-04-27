d61419bfe656b0c77d6645d39e27843e739eb3c1
saymyname <oakorotkov@alfabank.ru>
2021-04-28 02:24:23 +0300

    refactored

--- elirium-combination-lib/src/main/java/ru/smn/combination/strategy/KareSearchStrategy.java
+++ elirium-combination-lib/src/main/java/ru/smn/combination/strategy/KareSearchStrategy.java
@@ -1,50 +1,39 @@
 package ru.smn.combination.strategy;
 
+import ru.smn.combination.data.CardSizeData;
 import ru.smn.combination.data.CardType;
 import ru.smn.combination.data.Combination;
 import ru.smn.combination.data.CombinationType;
-import ru.smn.combination.data.PowerType;
+import ru.smn.combination.utils.CardUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static ru.smn.combination.data.CardSizeData.KARE_SIZE;
-import static ru.smn.combination.utils.CardUtils.filterByPower;
-import static ru.smn.combination.utils.CardUtils.findBiggerCardWithFilter;
 
 public class KareSearchStrategy implements SearchStrategy {
 
     @Override
     public Combination find(List<CardType> cards) {
-        final List<CardType> foundKare = new ArrayList<>();
+        final List<CardType> kareCards = cards.stream()
+                .collect(Collectors.groupingBy(CardType::getPowerAsInt))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().size() == CardSizeData.KARE_SIZE)
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.toList());
 
-        for (final CardType card : cards) {
-            final List<CardType> kare = filterByPower(cards, card.getPower());
-            if (kare.size() == KARE_SIZE) {
-                foundKare.addAll(kare);
-                break;
-            }
-        }
-
-        if (foundKare.isEmpty()) {
+        if (kareCards.isEmpty()) {
             return Combination.empty();
         }
 
-        final List<CardType> distinctKare = foundKare
-                .stream()
-                .distinct()
-                .collect(Collectors.toList());
+        final CardType kareCard = kareCards.get(0);
+        final CardType biggerCardWithoutKare = CardUtils.findBiggerCardWithFilter(cards, kareCard.getPower());
 
-        final PowerType powerOfKare = foundKare.get(0).getPower();
-        final CardType biggerCardWithFilter = findBiggerCardWithFilter(cards, powerOfKare);
-
-        distinctKare.add(biggerCardWithFilter);
+        kareCards.add(biggerCardWithoutKare);
 
         return Combination.of(
                 CombinationType.KARE,
-                distinctKare,
-                powerOfKare.getPowerAsInt() + biggerCardWithFilter.getPowerAsInt()
+                kareCards,
+                kareCard.getPowerAsInt() + biggerCardWithoutKare.getPowerAsInt()
         );
     }
 }
