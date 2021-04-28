5f2d235765f4bcd4ba2fbcbdb5b8b451b7ca5d85
saymyname <oakorotkov@alfabank.ru>
2021-04-28 12:23:22 +0300

    refactored

--- elirium-combination-lib/src/main/java/ru/smn/combination/strategy/ThreeCardsSearchStrategy.java
+++ elirium-combination-lib/src/main/java/ru/smn/combination/strategy/ThreeCardsSearchStrategy.java
@@ -3,11 +3,16 @@ package ru.smn.combination.strategy;
 import ru.smn.combination.data.CardType;
 import ru.smn.combination.data.Combination;
 import ru.smn.combination.data.CombinationType;
+import ru.smn.combination.utils.CardUtils;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static ru.smn.combination.data.CardSizeData.PAIR_SIZE;
 import static ru.smn.combination.data.CardSizeData.THREE_SIZE;
 import static ru.smn.combination.utils.CardUtils.*;
 
@@ -15,30 +20,33 @@ public class ThreeCardsSearchStrategy implements SearchStrategy {
 
     @Override
     public Combination find(List<CardType> cards) {
-        final List<CardType> foundThree = new ArrayList<>();
+        final List<CardType> threeCards = cards.stream()
+                .collect(Collectors.groupingBy(CardType::getPowerAsInt))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue().size() == THREE_SIZE)
+                .sorted(Map.Entry.<Integer, List<CardType>>comparingByKey().reversed())
+                .limit(1)
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.toList());
 
-        for (CardType card : cards) {
-            final List<CardType> three = filterByPower(cards, card.getPower());
+        if (threeCards.isEmpty()) {
+            return Combination.empty();
+        }
 
-            if (three.size() == THREE_SIZE) {
-                foundThree.addAll(three);
-            }
-        }
-        if (!foundThree.isEmpty()) {
-            for (int i = 0; i < 2; i++) {
-                final CardType biggerCard = findBiggerCardWithFilter(cards, foundThree.get(0).getPower());
-                foundThree.add(biggerCard);
-                cards.remove(biggerCard);
-            }
-            return Combination.of(
-                    CombinationType.THREE_CARDS,
-                    foundThree.stream()
-                            .distinct()
-                            .collect(Collectors.toList()),
-                    sumCards(foundThree)
-            );
-        }
-        return Combination.empty();
+        final List<CardType> highCards = cards.stream()
+                .filter(cardType -> !threeCards.contains(cardType))
+                .sorted(Comparator.comparingInt(CardType::getPowerAsInt).reversed())
+                .limit(2)
+                .collect(Collectors.toList());
+
+        final List<CardType> combination = Stream.concat(threeCards.stream(), highCards.stream())
+                .collect(Collectors.toList());
+
+        return Combination.of(
+                CombinationType.THREE_CARDS,
+                combination,
+                sumCards(threeCards)
+        );
 
     }
 }
